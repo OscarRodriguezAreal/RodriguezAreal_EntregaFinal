0144e26613a7b436aa7af6b255f9e57a7aef3b0f
Oscar Rodriguez Areal <orareal02@gmail.com>
2025-03-07 16:19:04 -0300

    subo el final

--- RodriguezAreal_EntregaFinal/settings.py
+++ RodriguezAreal_EntregaFinal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -46,7 +47,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ##'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'RodriguezAreal_EntregaFinal.urls'
@@ -54,8 +55,10 @@ ROOT_URLCONF = 'RodriguezAreal_EntregaFinal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [
+            BASE_DIR / 'templates',  # Esto incluye las plantillas globales
+        ],
+        'APP_DIRS': True,  # Asegura que busque plantillas dentro de las aplicaciones
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -115,9 +118,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
--- RodriguezAreal_EntregaFinal/urls.py
+++ RodriguezAreal_EntregaFinal/urls.py
@@ -16,7 +16,34 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+from blog import views  # Importa views desde el archivo actual
+from blog.views import BlogListView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),  # Vista 'Acerca de mí'
+    path('profile/', views.profile, name='profile'),  # Vista de perfil
+    path('blog/blog_lst', BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/new/', BlogCreateView.as_view(), name='blog_create'),
+    path('blog/<int:pk>/edit/', BlogUpdateView.as_view(), name='blog_edit'),
+    path('blog/<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit_profile, name='edit_profile'),
+    path('add-author/', views.add_author, name='add_author'),
+    path('add-category/', views.add_category, name='add_category'),
+    path('add-post/', views.add_post, name='add_post'),
+    path('buscar-post/', views.search_post, name='search_post'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
